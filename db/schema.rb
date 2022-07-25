1f0d2756e7caa582aceeb33c8b84a6b5adb4b20f
roibingdoelizondo99@gmail.com <roibingdoelizondo99>
2022-07-24 23:41:24 -0600

    Acept changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_07_24_184141) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
