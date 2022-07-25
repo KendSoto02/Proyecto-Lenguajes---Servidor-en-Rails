3dc233ce45efa75641c1d2f96f66f3db271e428e
roibingdoelizondo99@gmail.com <roibingdoelizondo99>
2022-07-24 22:48:31 -0600

    Clean template and API Dishes

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,12 @@ module AppComercial
   class Application < Rails::Application
     config.load_defaults 5.2
 
-<<<<<<< HEAD
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', headers: :any, methods: :any
       end
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-=======
->>>>>>> origin/feature/BGV-C03471
     config.assets.initialize_on_precompile = false
     I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
     I18n.default_locale = :es
